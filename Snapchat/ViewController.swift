20b28a1bf829b9975c1338d3e73fc802c429f833
victorsaicojusto <victor.saico@tecsup.edu.pe>
2018-05-23 14:33:02 -0500

    Initial Commit

---
+++ Snapchat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Snapchat
+//
+//  Created by victor saico justo on 23/05/18.
+//  Copyright © 2018 victor saico justo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
